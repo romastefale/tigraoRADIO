afecede8e909dd0cb0892fffcce4592f0af46d2f
@tigrao <romastefale@gmail.com>
2026-04-17 00:24:10 -0300

    Update intent.py

--- app/bot/intent.py
+++ app/bot/intent.py
@@ -4,7 +4,7 @@ def detect_intent(text: str) -> str | None:
 
     text = text.strip().lower()
 
-    if text in ["tocando", "tigraofm", "djpi", "royalfm", "geeksfm", "radinho", "qap"]:
+    if text in ["tocando", "cebrutius", "tigraofm", "djpi", "royalfm", "geeksfm", "radinho", "qap"]:
         return "play"
 
     return None
